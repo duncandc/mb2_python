15cdd368e6facc409315b16b655ec96f5c5016c7
Duncan Campbell <duncan@donut-hole.fios-router.home>
2019-07-11 11:08:32 -0400

    working

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# mb2_python
+# MassiveBlack-II Python
+
+This package contains functions to aid in calculations with [MassiveBlack-II](https://arxiv.org/abs/1402.0888) data products.
+
+
+## Description
+
+This package contains functions that load:
+
+* group particle data
+* subhalo particle data
+
+
+## Requirements
+
+In order to use the functions in this package, you will need the following Python packages installed:
+
+* [numpy](http://www.numpy.org)
+* [astropy](http://www.astropy.org)
+
+
+## Installation
+
+Place this directory in your PYTHONPATH.  The various functions can then be imported as, e.g.:
+
+```
+from mb2_python import load_subhalo
+```
+
+You can run the testing suite for this package using the [pytest](https://docs.pytest.org/en/latest/) framework by executing the following command in the package directory:
--- snapshot.py
+++ snapshot.py
@@ -3,10 +3,12 @@ File I/O related to the snapshot files.
 """
 
 import numpy as np
+import pygadgetreader
+
 
 def snapPath(basePath, snapNum, chunkNum=0):
     """ 
-    Return absolute path to a snapshot HDF5 file (modify as needed). 
+    Return absolute path to a snapshot file 
     """
     snapPath = basePath + '/snapdir'
     snapPath += '/snapdir_' + str(snapNum).zfill(3) + '/'
@@ -14,4 +16,148 @@ def snapPath(basePath, snapNum, chunkNum=0):
     filePath += '.' + str(chunkNum) + '.hdf5'
     return filePath
 
+
+def snapHeader(basePath, snapNum, chunkNum=0):
+	"""
+	Return snapshot header
+	"""
+	return pygadgetreader.readheader(snapPath(basePath, snapNum))
+
+
+def getNumPart(header):
+    """ 
+    Calculate number of particles of all types given a snapshot header. 
+    """
+    nTypes = 6
+
+    nPart = np.zeros(nTypes, dtype=np.int64)
+    for j in range(nTypes):
+        nPart[j] = header['NumPart_Total'][j] | (header['NumPart_Total_HighWord'][j] << 32)
+
+    return nPart
+
+
+def loadSubset(basePath, snapNum, partType, fields=None, subset=None, mdi=None, sq=True, float32=False):
+    """ 
+    Load a subset of fields for all particles/cells of a given partType.
+    If offset and length specified, load only that subset of the partType.
+    If mdi is specified, must be a list of integers of the same length as fields,
+    giving for each field the multi-dimensional index (on the second dimension) to load.
+    For example, fields=['Coordinates', 'Masses'] and mdi=[1, None] returns a 1D array
+    of y-Coordinates only, together with Masses.
+    If sq is True, return a numpy array instead of a dict if len(fields)==1.
+    If float32 is True, load any float64 datatype arrays directly as float32 (save memory). 
+    """
+
+    result = {}
+
+    ptNum = partTypeNum(partType)
+    gName = "PartType" + str(ptNum)
+
+    # make sure fields is not a single element
+    if isinstance(fields, six.string_types):
+        fields = [fields]
+
+    # load header from first chunk
+    with pygadgetreader.readheader(snapPath(basePath, snapNum)) as header:
+
+        nPart = getNumPart(header)
+
+        # decide global read size, starting file chunk, and starting file chunk offset
+        if subset:
+            offsetsThisType = subset['offsetType'][ptNum] - subset['snapOffsets'][ptNum, :]
+
+            fileNum = np.max(np.where(offsetsThisType >= 0))
+            fileOff = offsetsThisType[fileNum]
+            numToRead = subset['lenType'][ptNum]
+        else:
+            fileNum = 0
+            fileOff = 0
+            numToRead = nPart[ptNum]
+
+        result['count'] = numToRead
+
+        if not numToRead:
+            # print('warning: no particles of requested type, empty return.')
+            return result
+
+        # find a chunk with this particle type
+        i = 1
+        while gName not in f:
+            f = h5py.File(snapPath(basePath, snapNum, i), 'r')
+            i += 1
+
+        # if fields not specified, load everything
+        if not fields:
+            fields = list(f[gName].keys())
+
+        for i, field in enumerate(fields):
+            # verify existence
+            if field not in f[gName].keys():
+                raise Exception("Particle type ["+str(ptNum)+"] does not have field ["+field+"]")
+
+            # replace local length with global
+            shape = list(f[gName][field].shape)
+            shape[0] = numToRead
+
+            # multi-dimensional index slice load
+            if mdi is not None and mdi[i] is not None:
+                if len(shape) != 2:
+                    raise Exception("Read error: mdi requested on non-2D field ["+field+"]")
+                shape = [shape[0]]
+
+            # allocate within return dict
+            dtype = f[gName][field].dtype
+            if dtype == np.float64 and float32: dtype = np.float32
+            result[field] = np.zeros(shape, dtype=dtype)
+
+    # loop over chunks
+    wOffset = 0
+    origNumToRead = numToRead
+
+    while numToRead:
+        f = h5py.File(snapPath(basePath, snapNum, fileNum), 'r')
+
+        # no particles of requested type in this file chunk?
+        if gName not in f:
+            f.close()
+            fileNum += 1
+            fileOff  = 0
+            continue
+
+        # set local read length for this file chunk, truncate to be within the local size
+        numTypeLocal = f['Header'].attrs['NumPart_ThisFile'][ptNum]
+
+        numToReadLocal = numToRead
+
+        if fileOff + numToReadLocal > numTypeLocal:
+            numToReadLocal = numTypeLocal - fileOff
+
+        #print('['+str(fileNum).rjust(3)+'] off='+str(fileOff)+' read ['+str(numToReadLocal)+\
+        #      '] of ['+str(numTypeLocal)+'] remaining = '+str(numToRead-numToReadLocal))
+
+        # loop over each requested field for this particle type
+        for i, field in enumerate(fields):
+            # read data local to the current file
+            if mdi is None or mdi[i] is None:
+                result[field][wOffset:wOffset+numToReadLocal] = f[gName][field][fileOff:fileOff+numToReadLocal]
+            else:
+                result[field][wOffset:wOffset+numToReadLocal] = f[gName][field][fileOff:fileOff+numToReadLocal, mdi[i]]
+
+        wOffset   += numToReadLocal
+        numToRead -= numToReadLocal
+        fileNum   += 1
+        fileOff    = 0  # start at beginning of all file chunks other than the first
+
+        f.close()
+
+    # verify we read the correct number
+    if origNumToRead != wOffset:
+        raise Exception("Read ["+str(wOffset)+"] particles, but was expecting ["+str(origNumToRead)+"]")
+
+    # only a single field? then return the array instead of a single item dict
+    if sq and len(fields) == 1:
+        return result[fields[0]]
+
+    return result
 
