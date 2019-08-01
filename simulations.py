0c767f4d73f23ae137e92b3be4e1af442539e20d
Duncan Campbell <duncan@donut-hole.local>
2019-08-01 13:17:49 -0400

    updated

--- simulations.py
+++ simulations.py
@@ -8,7 +8,6 @@ import numpy as numpy
 cosmo = astropy.cosmology.FlatLambdaCDM(H0=70.2, Om0=0.275, Ob0=0.046)
 
 Lbox = 100.0  # In units of h^-1 Mpc
-Nptcl = 2*1792^3
-dm_ptcl_mass = 1.1*10^7  # Msol
-gas_ptcl_mass = 2.2*10^6  # Msol
+dm_ptcl_mass = 1.1*10^7  # Msol (scaled with h?)
+gas_ptcl_mass = 2.2*10^6  # Msol (scaled with h?)
 softening_length = 1.85  # h^-1 kpc
